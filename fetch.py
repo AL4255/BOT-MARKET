d1f58bd4d138396d6f9a8a12fa6952ac6b51cac1
AL4255 <92372109+AL4255@users.noreply.github.com>
2026-07-04 17:37:53 -0400

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -12,3 +12,15 @@ params = {"symbol": "MGNI", "token": apikey)
 url = "https://finhub.io/api/v1/quote"
 
 response = requests.get(url, params=params)
+
+data = response.json()
+price = data("c")
+print (price)
+
+everything = {
+  "symbol": "MGNI",
+  "price" : data["c"],
+  "high": data["h"],
+  "low": data["l"],
+  "prev_close": data["pc"]
+}
